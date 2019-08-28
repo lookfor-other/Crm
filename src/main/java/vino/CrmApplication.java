aaa7f0ed48118be3b92e3e1852d7659d3c4f13c6
Administrator <957222673@qq.com>
2019-08-28 22:26:23 +0800

    1、init project

---
+++ src/main/java/vino/CrmApplication.java
@@ -0,0 +1,19 @@
+package vino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 主启动类
+ * @Author: Vino
+ * @CreateDate: 2019/8/28 19:06
+ */
+
+@SpringBootApplication
+public class CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
+
+}
